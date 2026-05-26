8fbdba3aed8d2f9c2e4a75285c6de5f55e8c6133
ngamaj-est <nganaj@unicartagena.edu.co>
2026-05-25 22:49:20 -0500

    feat(domain): creacion de modelos pre-eliminares

---
+++ cea14_zoologico/src/main/java/com/example/unicartagena/cea14/domain/models/Zona.java
@@ -0,0 +1,10 @@
+package com.example.unicartagena.cea14.domain.models;
+
+import com.example.unicartagena.cea14.domain.exceptions.ZonaException;
+import com.example.unicartagena.cea14.domain.valueobjects.EspecieId;
+import com.example.unicartagena.cea14.domain.valueobjects.ExtensionTerreno;
+import com.example.unicartagena.cea14.domain.valueobjects.ZonaId;
+
+public class Zona {
+
+}
